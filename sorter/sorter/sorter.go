8761100aeaa953151ca267b89b8ee699d4dd7cfe
Aiello Chan <770540123@qq.com>
2016-11-04 00:05:38 +0800

    读取命令行输入并打印到控制台

---
+++ sorter/sorter/sorter.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+var (
+    infile *string = flag.String("i","infile","File contains values for sorting")
+    outfile *string = flag.String("o","outfile","File to resive sorted values")
+    algorithm *string = flag.String("a","qsort","Sort algorithm")
+)
+
+func main()  {
+    flag.Parse()
+
+    if infile != nil{
+        fmt.Println("infile = ", *infile, "outfile = ", *outfile, "algorithm = ", *algorithm)
+    }
+}
