41d0a073a32241b080bacc9c597740d7e0d17951
Aiello Chan <770540123@qq.com>
2016-11-13 17:15:46 +0800

    简单 http服务

---
+++ http/httpserve/src/httpserve.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+	"log"
+)
+
+func main() {
+    http.HandleFunc("/",func (w http.ResponseWriter, r *http.Request)  {
+        // fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+        w.Write([]byte("nihao"))
+    })
+
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
